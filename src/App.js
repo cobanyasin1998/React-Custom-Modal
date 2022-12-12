c0adfaa6b61d5dbc74a0b14b83e62b4b9a22ba1c
Yasin <yasincoban1998@outlook.com>
2022-12-12 22:48:33 +0300

    first init -> Redux and Tailwind Css added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./assets/css/index.css"
+
+function App() {
+  return (
+    <div className="App">
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </>
+    </div>
+  );
+}
+
+export default App;
